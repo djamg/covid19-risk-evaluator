91909d6f10702fdde1a8d5ef26263e10e4ce1eb5
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2020-09-14 19:15:17 +0530

    issues fixed

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/send', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def calculate():
     if request.method == 'POST':
         answer = riskcalculator.calculate(
             scene=int(request.form['scene']), mask=int(request.form['mask']), talk=int(request.form['talk']), distance=int(request.form['distance']))
         statement = "Risk of getting Covid-19 is " + \
-            str(answer[0]) + " in a million that is " + str(answer[1]) + "%"
+            str(answer[0]) + " in a Million that is " + str(answer[1]) + "%"
         return render_template('index.html', result=statement)
     else:
         return "error"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
